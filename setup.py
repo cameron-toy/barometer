ec1c70795ebe2410ad1b71ef51ff1d5c9cc84db1
Cameron Toy <cameron-toy@users.noreply.github.com>
2020-02-07 14:56:51 -0800

    added setup.py

---
+++ setup.py
@@ -0,0 +1,27 @@
+from distutils.core import setup
+
+setup(
+    name = 'barometer',
+    packages = ['barometer'],
+    version = '0.1',
+    license='MIT',
+    description = 'Simple logging library using print statements and decorators',
+    author = 'Cameron Toy',
+    author_email = 'cameron.toy.00@gmail.com',
+    url = 'https://github.com/cameron-toy/barometer',   # Provide either the link to your github or to your website
+    download_url = 'https://github.com/cameron-toy/barometer/archive/v0.1.tar.gz',
+    keywords = ['decorator', 'logging'],
+    install_requires=[],
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Build Tools',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+    ],
+)
